bbdd1acdf674096641720f713c80df442acb1642
Usra1 <usra.akbar123@gmail.com>
2019-07-04 10:59:30 +0500

    Updates

---
+++ precache-manifest.ad08aca1e4796db77dbeef050a846464.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "16a0be55a60963d337ee08feab591138",
+    "url": "/index.html"
+  },
+  {
+    "revision": "601485ce68739d7d81ea",
+    "url": "/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "b7097c61e4b7435cc055",
+    "url": "/static/css/main.fcabf51d.chunk.css"
+  },
+  {
+    "revision": "601485ce68739d7d81ea",
+    "url": "/static/js/2.9a4c80f6.chunk.js"
+  },
+  {
+    "revision": "b7097c61e4b7435cc055",
+    "url": "/static/js/main.ba71898b.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
